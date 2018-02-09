c67bf340898768a8d4965c597b5c33fff12f7c6d
Quyen Ha <quyenha2511@gmail.com>
2018-02-09 00:24:30 -0800

    add map

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,27 @@
+import _ from "lodash";
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, AsyncStorage } from "react-native";
 import Slides from "../components/Slides";
-
+import { AppLoading } from "expo";
 const SLIDE_DATA = [
   { text: "Welcome to Jobapp", color: "#03A9F4" },
   { text: "set your location then swipe away", color: "#009688" }
 ];
 
 class WelcomeScreen extends Component {
+  state = { token: null };
+
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem("fb_token");
+
+    if (token) {
+      this.props.navigation.navigate("map");
+      this.setState({ token });
+    } else {
+      this.setState({ token });
+    }
+  }
+
   // use onSlidesComplete() {} when you want to call .bind(this)
   // If not using .bind(this) we have to use this format
   onSlidesComplete = () => {
@@ -15,6 +29,9 @@ class WelcomeScreen extends Component {
   };
 
   render() {
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />;
+    }
     return <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />;
   }
 }
