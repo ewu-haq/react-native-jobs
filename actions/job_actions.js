a4875825215ea7bee7c519e65b8c35991bdaed67
Quyen Ha <qha@humanlongevity.com>
2018-02-09 17:52:15 -0800

    add search button, wire  up actions

---
+++ actions/job_actions.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import reverseGeocode from "latlng-to-zip";
+import { FETCH_JOBS } from "./type";
+import qs from "qs";
+
+const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
+const JOB_QUERY_PARAMS = {
+  publisher: "4201738803816157",
+  format: "json",
+  v: "2",
+  latlong: 1,
+  radius: 10,
+  q: "javascript" // hardcode type of jobs
+};
+
+const buildJobsUrl = zip => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
+export const fetchJobs = region => {
+  return async dispatch => {
+    try {
+      let zip = await reverseGeocode(region);
+      const url = buildJobsUrl(zip);
+      let { data } = await axios.get(url);
+      dispatch({ type: FETCH_JOBS, payload: data });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+};
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { View, Text, ActivityIndicator } from "react-native";
+import { Button } from "react-native-elements";
 import { MapView } from "expo";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class MapScreen extends Component {
   state = {
@@ -17,6 +20,14 @@ class MapScreen extends Component {
     this.setState({ mapLoaded: true });
   }
 
+  onRegionChangeComplete = region => {
+    this.setState({ region });
+  };
+
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  };
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -27,10 +38,32 @@ class MapScreen extends Component {
     }
     return (
       <View style={{ flex: 1 }}>
-        <MapView style={{ flex: 1 }} region={this.state.region} />
+        <MapView
+          style={{ flex: 1 }}
+          region={this.state.region}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: "search" }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: "absolute",
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+};
+
+export default connect(null, actions)(MapScreen);
